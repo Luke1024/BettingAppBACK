903e5071f718d52d219716b36ea8c161dc7fbefc
Luke1024 <chajdas.lukasz@gmail.com>
2019-08-22 16:42:54 +0200

    packages added

---
+++ src/main/java/com/betting/application/domain/UserDto.java
@@ -0,0 +1,14 @@
+package com.betting.application.domain;
+
+public class UserDto {
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+    private String matchingPassword;
+
+    private String email;
+
+    // standard getters and setters
+}
